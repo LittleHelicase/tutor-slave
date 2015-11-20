d2125bdb2970dec8e13c220e39e6289cfa1773e5
Tim Ebbeke <tebbeke@gdv.uni-hannover.de>
2015-11-20 16:13:58 +0100

    Removed memory database. It is no longer supported or maintained.

--- src/db.js
+++ src/db.js
@@ -1,5 +1,7 @@
 
 module.exports = function(config) {
+  // memory database is not maintained anymore
+  /*
   if(config.development && !config.devrdb){
     console.log("### development environment ###");
     return Promise.resolve(require("@tutor/memory-database")(config));
@@ -8,4 +10,8 @@ module.exports = function(config) {
       config.database.host + ":" + config.database.port + "/" + config.database.name + " ###")
     return (require("@tutor/rethinkdb-database"))(config);
   }
+  */
+  console.log("### development environment with RethinkDB @" +
+    config.database.host + ":" + config.database.port + "/" + config.database.name + " ###")
+  return (require("@tutor/rethinkdb-database"))(config);
 }
